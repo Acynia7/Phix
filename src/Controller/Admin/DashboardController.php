872d98a368c105711b284b1f85851f7c73ece2e1
Emilie Valentin <valentin.emilie78@gmail.com>
2024-10-02 14:28:02 +0200

    Adding CRUD

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Quiz;
+use App\Entity\QuizState;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(QuizCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('App');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
+        yield MenuItem::linkToCrud('Quiz', 'fas fa-map-marker-alt', Quiz::class);
+        yield MenuItem::linkToCrud('QuizState', 'fas fa-comments', QuizState::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-comments', User::class);
+    }
+}
---
+++ src/Controller/Admin/QuizStateCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\QuizState;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class QuizStateCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return QuizState::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Quiz State')
+            ->setEntityLabelInPlural('Quizzes State')
+            ->setSearchFields(['id'])
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('quizzes'))
+        ;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield AssociationField::new('quizzes');
+        yield IdField::new('id');
+    }
+}
